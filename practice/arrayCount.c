845059cc331e98b780ceb740c80f4899437b9846
Sandesh Dadibude <129705613+Dadibudesandesh@users.noreply.github.com>
2024-03-06 00:26:00 +0530

    Add files via upload

---
+++ practice/arrayCount.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int arr[10],count,i,size;
+    char car[10];
+    float f[10];
+    double d[10];
+
+    printf("Enter size of array : ");
+    scanf("%d",&size);
+    for(i=0;i<size;i++)
+    {
+        count++;
+    }
+    printf("\nsize array :  %d",count);
+    /*printf("\nSize char : %d",sizeof(car));
+    printf("\nSize float : %d",sizeof(f));
+    printf("\nSize double : %d",sizeof(d));
+*/
+
+
+}
